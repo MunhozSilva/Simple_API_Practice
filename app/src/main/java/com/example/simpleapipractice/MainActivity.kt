0b4b158c2569c52d266d0f215867ac863d844429
MunhozSilva <luciano.asimov@gmail.com>
2022-03-31 09:05:22 -0300

    Handling exceptions

--- app/src/main/java/com/example/simpleapipractice/MainActivity.kt
+++ app/src/main/java/com/example/simpleapipractice/MainActivity.kt
@@ -3,27 +3,39 @@ package com.example.simpleapipractice
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.TextView
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.example.simpleapipractice.databinding.ActivityMainBinding
 import com.example.simpleapipractice.repository.Repository
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var viewModel: MainViewModel
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
         val repository = Repository()
         val viewModelFactory = MainViewModelFactory(repository)
         viewModel = ViewModelProvider(this,viewModelFactory).get(MainViewModel::class.java)
         viewModel.getPost()
         viewModel.myResponse.observe(this, Observer { response ->
-            Log.d("Response", response.userID.toString())
-            Log.d("Response", response.id.toString())
-            Log.d("Response", response.title)
-            Log.d("Response", response.body)
+            if (response.isSuccessful) {
+                Log.d("Response", response.body()?.userID.toString())
+                Log.d("Response", response.body()?.id.toString())
+                binding.textView.text = response.body()?.title!!
+                //Log.d("Response", response.body()?.title!!)
+                Log.d("Response", response.body()?.body!!)
+            } else{
+                Log.d("Response", response.errorBody().toString())
+                binding.textView.text = response.code().toString()
+            }
         })
     }
 }
